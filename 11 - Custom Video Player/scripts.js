d867041f51595eb4ed9bbe793083b8b11453af14
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-19 18:40:18 -0500

    Calculates percent for click and drag on scrub bar

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,47 @@
+
+window.onload = (e) => {
+  let video = document.querySelector('video');
+  let timestamp = video.currentTime;
+  let progressBar = document.querySelector('.progress');
+
+  let scrubSetup = () => {
+    progressBar.addEventListener('mousemove', scrub);
+    // document.addEventListener('mousemove', scrub); // TODO: why does this not work ?? along with #2
+  }
+
+  let scrub = (e) => {
+
+    console.log(calculatePercent(e))
+    // console.log('clientX / srcElement.clientWidth: ', e.clientX / e.srcElement.clientWidth); // TODO: why do these not calculate the percent correctly??
+    // console.log('e.x / e.srcElement.clientWidth: ', e.x, e.srcElement.clientWidth);
+
+    if (e.currentTarget === progressBar) {
+
+    }
+  }
+
+  let calculatePercent = (e) => {
+    const screenX = e.screenX;
+    const progressBarWidth = progressBar.clientWidth;
+    const innerWidth = window.innerWidth;
+    const margin = (innerWidth - progressBarWidth) / 2;
+
+    return (screenX - margin) / progressBarWidth;
+  }
+
+  document.addEventListener('mousedown', scrubSetup);
+
+  document.addEventListener('mouseup', () => {
+    progressBar.removeEventListener('mousemove', scrub);
+    // document.removeEventListener('mousemove', scrub); // TODO #2
+  });
+
+}
+
+// on mousedown on the scrub bar
+  // add event listener on mousemove
+    // each mousemove event calculates the current position as a percentage of the div's width
+    // seeks the video to that percent
+    // updates the visual to display that percent of the scrub bar filled
+// on mouseup on the scrub bar
+  // removes that event listener
